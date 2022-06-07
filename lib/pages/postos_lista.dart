bcb8146ecfa465abd5bf5ee05beb75a34fd8e426
WalterLimaViana <walter.lima.viana@gmail.com>
2022-06-07 20:34:31 -0300

    created body posto_lista page

--- lib/pages/postos_lista.dart
+++ lib/pages/postos_lista.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:postos_local/models/posto.dart';
 import 'package:postos_local/repositories/postos_repository.dart';
 
 class PostosLista extends StatefulWidget {
@@ -9,11 +10,25 @@ class PostosLista extends StatefulWidget {
 }
 
 class _PostosListaState extends State<PostosLista> {
+  final List<Posto> lista = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('Lista de postos')),
+      ),
+      body: ListView.builder(
+        itemCount: lista.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+              Text('${lista[index].nome}'),
+              Text('${lista[index].endereco}'),
+              Text('${lista[index].foto}'),
+            ],
+          );
+        },
       ),
     );
   }
